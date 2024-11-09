c19ab89d4e0ff2dfb3512e6819c336f7c02b7b56
Jeetp45 <141786979+Jeetp45@users.noreply.github.com>
2024-11-08 20:35:03 -0500

    added box and name input to intro page

--- client/components/IntroPage.jsx
+++ client/components/IntroPage.jsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const IntroPage = () => {
   const navigate = useNavigate();
 
+  const [username, setUsername] = useState('');
+  const instructionString = '';
+  const tipString = '';
+
   return (
-    <div>
-      <h1>Intro Page</h1>
+    <div
+      style={{
+        border: '2px solid black',
+        padding: '20px',
+        borderRadius: '8px',
+        width: '1000px',
+        height: '800px',
+        margin: '50px auto',
+        backgroundColor: 'white',
+        textAlign: 'center',
+      }}
+    >
+      <h1>Welcome to AImongus!</h1>
+      <p>hi</p>
+      <input
+        type='text'
+        placeholder='Enter Your Name'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        style={{
+          fontSize: '20px',
+          padding: '10px 20px',
+          marginTop: '20px',
+          textAlign: 'center',
+        }}
+      />
       <div>
         <button
           onClick={() => navigate('/level1')}
+          //want to save username to leaderboard when we click continue
           style={{ fontSize: '20px', padding: '10px 20px', marginTop: '20px' }}
         >
           Continue
         </button>
-      </div>{' '}
+      </div>
     </div>
   );
 };
